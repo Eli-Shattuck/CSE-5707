8753040e77968e9acd4cdff87a8694d22044cba8
Eli-Shattuck <73374807+Eli-Shattuck@users.noreply.github.com>
2022-10-03 15:29:38 -0400

    more work on q2

--- HW2/planning.py
+++ HW2/planning.py
@@ -7,7 +7,14 @@ cpx.solve()
 sol = cpx.solution.get_values(0, cpx.variables.get_num()-1)
 
 k=0
-for i in range(2):
-    for j in range(6):
-        print("{}_{} = {}".format('UP'[i], j+3, sol[k  ]))       
-        k += 1
+for j in range(6):
+    print("P_{} = {}".format(j+3, sol[k  ]))       
+    k += 1
+
+for j in range(5):
+    print("S_{} = {}".format(j+3, sol[k  ]))       
+    k += 1
+
+for j in range(5):
+    print("R_{} = {}".format(j+4, sol[k  ]))       
+    k += 1
---
+++ HW2/thinking.md
@@ -0,0 +1,14 @@
+Pₓ is the amount prodced at month x.
+Dₓ is the demand for month x.
+Sₓ is the amount stored at the end of month x.
+Rₓ is the amount ruined at the end of month x.
+
+We minimize
+
+15 ∑ Pₓ + .75 ∑ Sₓ + 25 ∑ Rₓ
+
+subject to Pₓ ≥ Dₓ.
+
+In month 3, we have 1950u saved. D₃ = 5000, D₃ > S₃ there wil be no units from month < 3 stored at month = 4. Thus Sₓ = Pₓ + .89 Sₓ₋₁ - Dₓ ∀x > 3.
+
+Also, the amount ruined each month is Rₓ = .11 Sₓ₋₁ ∀x > 3.
